bdaef8ac24a23a78d43f9f3c280a60d4d409b9f8
latif <latif@robomotion.io>
2023-12-09 15:02:42 +0300

    .

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 RSA="xebula-rsa"
 NAME="xebula"
+HOSTNAME="xebula.online"
+CRT="xebula-crt"
 
 #kubectl
 echo "Installling kubectl..."
@@ -22,18 +24,42 @@ rm -rf helm-v${HELM}-linux-amd64.tar.gz
 echo "Installing microk8s"
 snap remove microk8s
 snap install microk8s --classic
-microk8s.enable dns rbac storage ingress
+microk8s.enable dns
+microk8s.enable rbac
+microk8s.enable storage
+microk8s.enable ingress
 microk8s.config view > $HOME/.kube/config
+
+kubectl annotate ingressclass nginx meta.helm.sh/release-name=${NAME}
+kubectl annotate ingressclass nginx meta.helm.sh/release-namespace=default
+kubectl label ingressclass nginx app.kubernetes.io/managed-by=Helm
+
+
+echo "Installing cert-manager"
+#kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.7.2/cert-manager.crds.yaml
+#kubectl label namespace default certmanager.k8s.io/disable-validation="true"
+
+# echo "Creating self-signed cert"
+echo "[req]" > crt.config
+echo "distinguished_name=req" >> crt.config
+echo "[san]" >> crt.config
+echo "subjectAltName=DNS:${HOSTNAME}" >> crt.config
+openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes -keyout rpa.pem -out rpa.crt -subj "/CN=${HOSTNAME}" -extensions san -config crt.config
+rm crt.config
+
+# echo "Installing TLS certificate"
+kubectl create secret tls ${CRT} --cert=./rpa.crt --key=./rpa.pem
+rm rpa.crt rpa.pem
+
 
 #buralar düzeltilecek 
 kubectl create -f /home/latif/temp/sendgrid-secret.yaml
-kubectl create -f /home/latif/temp/jwt-secret.yaml
 
 
 # Create keycloak secret
-echo "Generating keycloak secret" 
-keycloak=$(openssl rand -hex 16)
-kubectl create secret generic keycloak --from-literal=secret="$keycloak"
+# echo "Generating keycloak secret" 
+# keycloak=$(openssl rand -hex 16)
+# kubectl create secret generic keycloak --from-literal=secret="$keycloak"
 
 echo "Creating RSA keys"
 openssl genrsa -out rsa.private 2048
@@ -42,7 +68,4 @@ kubectl create secret generic ${RSA} --from-file=privatekey=rsa.private --from-f
 rm rsa.private rsa.public
 
 echo "Installing charts"
-cd /home/latif
-echo oldu
-echo ${NAME}
 helm install ${NAME} . -f values.yaml
